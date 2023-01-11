3037d335670f475f93d5c597386bdea3c823ab39
Anshu Naagar <108215633+i-ice-bear@users.noreply.github.com>
2023-01-11 06:47:07 +0000

    Pending changes exported from your codespace

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
-import '../global.css'
+import React, { Component, PropTypes } from "react";
+import HomeComponent from "./index";
+import { Container } from "@nextui-org/react";
+import NavbarComponent from "../Router/Navbar";
+import "../global.css";
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { createTheme, NextUIProvider } from "@nextui-org/react"
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+     // optional
+  }
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    // optional
+  }
+})
+
+export default class AppComponent extends Component {
+  render() {
+    return (
+      <NextThemeProvider 
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <NavbarComponent />
+        </NextUIProvider>
+      </NextThemeProvider>
+    );
+  }
 }
