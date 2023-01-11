a07ac104294c10c0e714e2618fbd479d8ab5e63b
Anshu Naagar <108215633+i-ice-bear@users.noreply.github.com>
2023-01-11 06:48:57 +0000

    AGenerated navbar for spaceCode

---
+++ components/Switch/Switch.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Switch } from "@nextui-org/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useTheme as useNextTheme } from "next-themes";
+import { useTheme } from "next-themes";
+
+export default function SwitchComponent() {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+  return (
+    <Switch
+      color="error"
+      css={{ maxWidth: "$12rem" }}
+      checked={isDark}
+      onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+      iconOn={<FaSun />}
+      iconOff={<FaMoon />}
+    ></Switch>
+  );
+}
